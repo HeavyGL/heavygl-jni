bb1bf68ceab37c33e89197ca11a1a0d7465019b1
Ciro Diaz <cirod307@gmail.com>
2024-11-13 21:30:17 +0100

    ♥️ Do a square rotating

--- modules/lwjgl3/src/main/java/org/josl/heavygl/util/GLRaster.java
+++ modules/lwjgl3/src/main/java/org/josl/heavygl/util/GLRaster.java
@@ -7,22 +7,37 @@ package org.josl.heavygl.util;
 import static org.josl.heavygl.HGL11.*;
 import static org.lwjgl.system.MemoryUtil.*;
 
+import java.awt.Dimension;
 import java.awt.image.BufferedImage;
 import java.awt.image.DataBufferInt;
 import java.nio.IntBuffer;
 
-public class GLRaster {
+public final class GLRaster {
 
-	public final int width, height;
-	public IntBuffer pixels;
-	public long address;
+	private static GLRaster shared;
+	
+	public int width, height;
+	IntBuffer pixels;
 
-	public GLRaster(int width, int height) {
+	GLRaster(int width, int height) {
+		size(width, height);
+	}
+
+	private void allocate() {
+		free();
+		this.pixels = memAllocInt(width * height);
+		makeContextCurrent();
+	}
+	
+	public GLRaster size(int width, int height) {
+		if (this.width == width && this.height == height)
+			return this;
+		
 		this.width = width;
 		this.height = height;
-
-		this.pixels = memAllocInt(width * height);
-		this.address = memAddress(pixels);
+		this.allocate();
+		
+		return this;
 	}
 
 	public void copy(BufferedImage bi) {
@@ -35,8 +50,27 @@ public class GLRaster {
 		memFree(pixels);
 	}
 
-	public void makeCurrentContext() {
+	private void makeContextCurrent() {
 		glXSetContext(pixels, width, height);
+	}
+	
+	public static GLRaster get(int width, int height) {
+		if (shared == null)
+			return GLRaster.create(width, height);
+		
+		return shared.size(width, height);
+	}
+	
+	public static GLRaster create(int width, int height) {
+		return shared = new GLRaster(width, height);
+	}
+
+	public static GLRaster get(Dimension size) {
+		return GLRaster.get(size.width, size.height);
+	}
+
+	public BufferedImage createCompatibleImage() {
+		return new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
 	}
 
 }
